4a4184df6fa0fc06a2ff9150264f8c0171bd50b3
fanchong02 <fanchong02@meituan.com>
2020-05-24 16:27:59 +0800

    feat: 无损压缩工具

---
+++ README.md
@@ -0,0 +1,13 @@
+# img-minify-tool
+#### 一行命令即可实现所有图片无损压缩， 大大提高工作效率
+
+说明： 执行 `minify.sh`脚本， 传入要压缩图片的目录路径即可。
+
+使用方法：
+1. 保存minify.sh到你的本地或服务器
+2. 执行minify.sh 脚本
+示例：
+```
+sh minify.sh /Users/xxx/xxx/yourProject;
+```
+即可启动图片无损压缩任务
---
+++ index.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const fs = require('fs');
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminSvgo = require('imagemin-svgo');
+const imageminGifsicle = require('imagemin-gifsicle');
+
+const argv = process.argv.filter(item => /^catalogPath\=/.test(item))[0];
+//如果未传入指定参数，提示， 并终止任务
+if (!argv) {
+  console.log('请检查是否注入了catalogPath参数（示例：node index.js catalogPath=/Users/xxx/xxx/项目目录）');
+  return;
+}
+//获取执行任务的路径
+const catalogPath = argv.split('=')[1];
+console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>开始执行图片无损压缩任务>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n');
+console.log(`本次无损压缩任务目录为：${catalogPath}\n`);
+//图片无损压缩函数
+(async () => {
+  //存放压缩前的图片体积
+  let beforeSizeList = [];
+  //存放压缩后的图片体积
+  let afterSizeList = [];
+  //字节转kb方法
+  const byteToKb = (byte) => {
+    return `${(byte / 1024).toFixed(2)}kb`;
+  }
+  //遍历某个文件夹下所有的XX文件
+  async function findImageAndLetItSmall({
+    catalogPath,
+    EXT_NAME = /^\.(jpg|jpeg|png|gif|svg|webp)$/i
+  }) {
+    const pathList = fs.readdirSync(catalogPath);
+    for (let i = 0; i < pathList.length; i++) {
+      //生成子文件、子目录的路径， 等待二次遍历确认
+      const childCatalogOrFilePath = path.join(catalogPath, pathList[i]);
+      //根据文件路径获取文件信息，返回一个fs.Stats对象
+      const statInfo = fs.statSync(childCatalogOrFilePath);
+      if (statInfo.isDirectory()) {
+        await findImageAndLetItSmall({
+          catalogPath: childCatalogOrFilePath,
+          EXT_NAME
+        });
+      } else {
+        if (EXT_NAME.test(path.extname(childCatalogOrFilePath))) {
+          //如果此文件为图片，对此文件进行压缩替换
+          const beforeSize = fs.statSync(childCatalogOrFilePath).size;
+          //统计压缩前size
+          beforeSizeList = [...beforeSizeList, beforeSize];
+          console.log(`${childCatalogOrFilePath} => 正在无损压缩(压缩前体积：${byteToKb(beforeSize)})`);
+          await imagemin([childCatalogOrFilePath], {
+            destination: catalogPath,
+            plugins: [
+              imageminJpegtran(),
+              imageminPngquant({
+                quality: [0.6, 0.7]
+              }),
+              imageminSvgo(),
+              imageminGifsicle()
+            ]
+          });
+          const afterSize = fs.statSync(childCatalogOrFilePath).size;
+
+          afterSizeList = [...afterSizeList, afterSize];
+          console.log(`${childCatalogOrFilePath} => 压缩完成（压缩后体积：${byteToKb(afterSize)}）\n`);
+        }
+      }
+    }
+  }
+
+  await findImageAndLetItSmall({
+    catalogPath
+  });
+  //压缩前总体积
+  const beforeSizeCount = beforeSizeList.reduce((cur, sum) => cur + sum);
+  //压缩后总体积
+  const afterSizeCount = afterSizeList.reduce((cur, sum) => cur + sum);
+  //压缩掉的体积
+  const saveCount = beforeSizeCount - afterSizeCount;
+
+  console.log(`===========> 压缩前总体积${byteToKb(beforeSizeCount)}`);
+  console.log(`===========> 压缩后总体积${byteToKb(afterSizeCount)}`);
+  console.log(`===========> 节约体积${(saveCount * 100 / beforeSizeCount).toFixed(2)}%（共${byteToKb(saveCount)}）\n\n`);
+  console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<图片无损压缩任务执行完毕<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+})();
